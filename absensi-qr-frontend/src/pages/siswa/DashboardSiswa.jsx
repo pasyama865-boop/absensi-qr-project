5014b6088588bd9457fd8522ec37c6f541a546f2
Muhamad ali pasha <pasyama865@gmail.com>
2025-12-13 10:21:00 +0700

    commit

--- absensi-qr-frontend/src/pages/siswa/DashboardSiswa.jsx
+++ absensi-qr-frontend/src/pages/siswa/DashboardSiswa.jsx
@@ -1,13 +1,12 @@
-// src/pages/admin/DashboardAdmin.jsx
 import React from 'react';
 
-const DashboardAdmin = () => {
+const DashboardSiswa = () => {
     return (
         <div className="p-4">
-            <h2 className="text-3xl font-bold text-gray-800">Dashboard Admin</h2>
+            <h2 className="text-3xl font-bold text-gray-800">Dashboard Siswa</h2>
             <p className="mt-2 text-gray-600">Selamat datang di panel administrasi utama.</p>
         </div>
     );
 };
 
-export default DashboardAdmin;
+export default DashboardSiswa;
